44ebdea81ed0c2af16ba1a91359a537922022870
shreyathanmaya <67581647+shreyathanmaya@users.noreply.github.com>
2022-12-06 09:01:31 +0530

    Add files via upload

---
+++ NumberOfDigits.java
@@ -0,0 +1,13 @@
+public class NumberOfDigits
+{
+	public static void main(String[] args)
+	{
+		int count = 0, num=0;
+		while( num != 0)
+		{
+			num = num/10;
+			++count;
+		}
+		System.out.println("Number of digits: " + count);
+	}
+}
