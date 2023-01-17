2c2d81f67115e1c3aa25cb09ffeb53e9dd936734
shreyathanmaya <67581647+shreyathanmaya@users.noreply.github.com>
2023-01-17 09:16:10 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+package Java;
+public class SimpleInterest 
+{
+	public static void main(String args[])
+	{
+			float P = 10000, R = 5, T = 5;
+			float SI = (P * T * R) / 100;
+			System.out.println("Simple interest = " + SI);
+		
+	}
+}
