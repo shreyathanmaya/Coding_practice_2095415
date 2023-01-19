3839397409cf2668e4832bafbe1801d0f02d6ff8
shreyathanmaya <67581647+shreyathanmaya@users.noreply.github.com>
2023-01-19 09:40:17 +0530

    Add files via upload

---
+++ RightTriangle.java
@@ -0,0 +1,22 @@
+package Java;
+import java.io.*;
+public class RightTriangle
+{
+	public static void StarRightTriangle(int n)
+	{
+		int a,b;
+		for(a=0; a<n; a++)
+		{
+			for(b =0; b<=a; b++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args)
+	{
+		int k =5;
+		StarRightTriangle(k);
+	}
+}
