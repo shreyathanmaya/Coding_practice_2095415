464966f5e82f8390cb63ac2197bb847676affb51
shreyathanmaya <67581647+shreyathanmaya@users.noreply.github.com>
2023-02-23 09:38:11 +0530

    Add files via upload

---
+++ ArraysAreEqualOrNot.java
@@ -0,0 +1,23 @@
+package Java;
+import java.util.Arrays;
+public class ArraysAreEqualOrNot
+{
+	public static void main(String[] args)
+	{
+			int a[] = { 30, 25, 40};
+
+			int b[] = { 30, 25, 40};
+
+			boolean result = Arrays.equals(a, b);
+			if (result == true)
+			{
+				System.out.println("Two arrays are equal");
+			}
+			else 
+			{
+				System.out.println("Two arrays are not equal");
+			}
+		
+	}
+
+}
